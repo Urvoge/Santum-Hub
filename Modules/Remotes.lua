1aee4f14503f081672ccf02460db47984c321d5d
Urvoge <75653742+Urvoge@users.noreply.github.com>
2021-04-24 10:10:00 -0400

    Delete Remotes.lua

--- Modules/Remotes.lua
+++
@@ -1,8 +0,0 @@
-return {
-    ['remote_proxy'] = game.ReplicatedStorage.Interaction.RemoteProxy,
-    ['client_interacted'] = game.ReplicatedStorage.Interaction.ClientInteracted,
-    ['destroy_structure'] = game.ReplicatedStorage.Interaction.DestroyStructure,
-    ['client_is_dragging'] = game.ReplicatedStorage.Interaction.ClientIsDragging,
-    ['client_is_whitelisted'] = game.ReplicatedStorage.Interaction.ClientIsWhitelisted,
-    ['client_request_ownership'] = game.ReplicatedStorage.Interaction.ClientRequestOwnership,
-}
