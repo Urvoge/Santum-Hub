386104deda247971134c6bf109dac77fc156b0d5
Urvoge <75653742+Urvoge@users.noreply.github.com>
2021-04-24 10:15:37 -0400

    Update Remotes.lua

--- Modules/Lumber_Tycoon_2/Remotes.lua
+++ Modules/Lumber_Tycoon_2/Remotes.lua
@@ -1,8 +1,10 @@
 return {
-    ['remote_proxy'] = game.ReplicatedStorage.Interaction.RemoteProxy,
-    ['client_interacted'] = game.ReplicatedStorage.Interaction.ClientInteracted,
-    ['destroy_structure'] = game.ReplicatedStorage.Interaction.DestroyStructure,
-    ['client_is_dragging'] = game.ReplicatedStorage.Interaction.ClientIsDragging,
-    ['client_is_whitelisted'] = game.ReplicatedStorage.Interaction.ClientIsWhitelisted,
-    ['client_request_ownership'] = game.ReplicatedStorage.Interaction.ClientRequestOwnership,
+    ["remote_proxy"] = game.ReplicatedStorage.Interaction.RemoteProxy,
+    ["client_interacted"] = game.ReplicatedStorage.Interaction.ClientInteracted,
+    ["destroy_structure"] = game.ReplicatedStorage.Interaction.DestroyStructure,
+    ["client_is_dragging"] = game.ReplicatedStorage.Interaction.ClientIsDragging,
+    ["client_is_whitelisted"] = game.ReplicatedStorage.Interaction.ClientIsWhitelisted,
+    ["client_request_ownership"] = game.ReplicatedStorage.Interaction.ClientRequestOwnership,
+    ["client_expanded_property"] = game.ReplicatedStorage.PropertyPurchasing.ClientExpandedProperty,
+    ["client_purchase_property"] = game.ReplicatedStorage.PropertyPurchasing.ClientPurchasedProperty,
 }
